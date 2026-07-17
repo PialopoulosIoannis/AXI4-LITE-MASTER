49f9205a1cbd10267642a6f6ad4b6c6a48e6f2ae
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-07-17 14:04:52 +0300

    Fix signal initialization and correct variable assignments in read process

--- axi4lt_slave.vhdl
+++ axi4lt_slave.vhdl
@@ -38,22 +38,22 @@ entity axi4_lite_master is
 end axi4_lite_master;
 
 architecture behavioural of axi4_lite_master is
-    signal src_base_addr : std_logic_vector(ADDR_WIDTH-1 downto 0) <= "00000000";
-    signal length_in_bytes : std_logic_vector((NB_COL * COL_WIDTH)-1 downto 0) <= b"00000000000000000000000000000001";
+    signal src_base_addr : std_logic_vector(ADDR_WIDTH-1 downto 0) := "00000000";
+    signal length_in_bytes : std_logic_vector((NB_COL * COL_WIDTH)-1 downto 0) := b"00000000000000000000000000000001";
     signal internal_arvalid : std_logic;
     signal internal_rready : std_logic;
     type STATES is (IDLE,READ,FINAL_READ,END);
     signal state : STATES:= IDLE;
-    signal counter : integer := '0';
-    signal how_many_reads : integer;
-    signal final_read : integer;
-    signal bytes_in_int : integer;
     type ram_type is array (0 to SIZE - 1) of std_logic_vector(NB_COL * COL_WIDTH - 1 downto 0);
     signal mydata : ram_type := (others => (others => '0'));
 
 
     process(areset_n,aclk) --READ
     begin
+    variable counter : integer := 0;
+    variable how_many_reads : integer;
+    variable final_read : integer;
+    variable bytes_in_int : integer;
 
         if areset_n = '0' then 
            internal_arvalid <= '0';
@@ -62,15 +62,15 @@ architecture behavioural of axi4_lite_master is
         elsif rising_edge(aclk) then
           case (state) is
            when IDLE =>   
-                bytes_in_int:= to_integer(unsigned(lenght_in_bytes));
-                how_many_read:= bytes_in_int div 4;
+                bytes_in_int:= to_integer(unsigned(length_in_bytes));
+                how_many_reads:= bytes_in_int div 4;
                 final_read:= bytes_in_int mod 4; 
                 s_axilt_araddr <= src_base_addr;
                 state <= READ;
            when READ =>
                 if s_axilt_araddr /= "11111111" then
                 internal_arvalid <= '1';
-                s_axilt_rready <= '1';
+                internal_rready <= '1';
             end if;
             if internal_arvalid = '1' and s_axilt_arready = '1' then
                 internal_arvalid <= '0';
@@ -78,15 +78,15 @@ architecture behavioural of axi4_lite_master is
             if s_axilt_rvalid = '1' and internal_rready = '1' then 
                 internal_rready <= '0';
                 mydata(counter) <= s_axilt_rdata;
-                counter <= counter + 1;
+                counter := counter + 1;
                 if counter /= how_many_reads then
                     state <= READ;
-                    s_axilt_araddr <= src_base_addr + counter*4;
+                    s_axilt_araddr <= std_logic_vector(unsigned(src_base_addr) + to_unsigned(counter * 4, ADDR_WIDTH));
                 elsif final_read = '0' then
                     state <= END;
                 else 
                 state <= FINAL_READ;
-                 s_axilt_araddr <= src_base_addr + counter*4;
+                 s_axilt_araddr <= std_logic_vector(unsigned(src_base_addr) + to_unsigned(counter * 4, ADDR_WIDTH));
             end if;
             when FINAL_READ => 
                 if s_axilt_araddr /= "11111111" then
@@ -99,7 +99,7 @@ architecture behavioural of axi4_lite_master is
                 if s_axilt_rvalid = '1' and internal_rready = '1' then 
                     internal_rready <= '0';
                     mydata(counter) <= s_axilt_rdata((final_read*8)-1 downto 0);
-                    counter <= counter + 1;
+                    counter := counter + 1;
                     state <= END;
                 end if;
             when END =>
@@ -107,6 +107,7 @@ architecture behavioural of axi4_lite_master is
                     for i in 0 to counter-1 loop 
                         report "DATA:" &to_string(mydata(i));
                     end loop; 
+                    wait;
             end case; 
             end if;
             end process;
