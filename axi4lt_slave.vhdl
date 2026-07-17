1dd68e4288c11f6e11ed910030e3b13732a9c2f8
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-07-17 13:46:04 +0300

    Refactor read process to handle byte length and implement state machine for data retrieval

--- axi4lt_slave.vhdl
+++ axi4lt_slave.vhdl
@@ -39,34 +39,77 @@ end axi4_lite_master;
 
 architecture behavioural of axi4_lite_master is
     signal src_base_addr : std_logic_vector(ADDR_WIDTH-1 downto 0) <= "00000000";
-    signal length_in_bits : std_logic_vector((NB_COL * COL_WIDTH)-1 downto 0) <= b"00000000000000000000000000000001";
-    signal mydata : std_logic_vector((NB_COL * COL_WIDTH)-1 downto 0) <= (others => '0'); 
+    signal length_in_bytes : std_logic_vector((NB_COL * COL_WIDTH)-1 downto 0) <= b"00000000000000000000000000000001";
     signal internal_arvalid : std_logic;
     signal internal_rready : std_logic;
+    type STATES is (IDLE,READ,FINAL_READ,END);
+    signal state : STATES:= IDLE;
+    signal counter : integer := '0';
+    signal how_many_reads : integer;
+    signal final_read : integer;
+    signal bytes_in_int : integer;
+    type ram_type is array (0 to SIZE - 1) of std_logic_vector(NB_COL * COL_WIDTH - 1 downto 0);
+    signal mydata : ram_type := (others => (others => '0'));
 
 
     process(areset_n,aclk) --READ
     begin
+
         if areset_n = '0' then 
            internal_arvalid <= '0';
             s_axilt_awvalid <= '0';
             s_axilt_wvalid <= '0';
         elsif rising_edge(aclk) then
-            if src_base_addr /= "11111111" then
+          case (state) is
+           when IDLE =>   
+                bytes_in_int:= to_integer(unsigned(lenght_in_bytes));
+                how_many_read:= bytes_in_int div 4;
+                final_read:= bytes_in_int mod 4; 
+                s_axilt_araddr <= src_base_addr;
+                state <= READ;
+           when READ =>
+                if s_axilt_araddr /= "11111111" then
                 internal_arvalid <= '1';
-                s_axilt_rready =<='1';
+                s_axilt_rready <= '1';
             end if;
             if internal_arvalid = '1' and s_axilt_arready = '1' then
                 internal_arvalid <= '0';
-                src_base_addr <="11111111";
             end if;
             if s_axilt_rvalid = '1' and internal_rready = '1' then 
                 internal_rready <= '0';
-                mydata <= s_axilt_rdata(length_in_bytes-1 downto 0);
-                report "DATA: " & to_string(mydata);
+                mydata(counter) <= s_axilt_rdata;
+                counter <= counter + 1;
+                if counter /= how_many_reads then
+                    state <= READ;
+                    s_axilt_araddr <= src_base_addr + counter*4;
+                elsif final_read = '0' then
+                    state <= END;
+                else 
+                state <= FINAL_READ;
+                 s_axilt_araddr <= src_base_addr + counter*4;
             end if;
-        end if;
-    end process;
+            when FINAL_READ => 
+                if s_axilt_araddr /= "11111111" then
+                internal_arvalid <= '1';
+                s_axilt_rready <= '1';
+                end if;
+                if internal_arvalid = '1' and s_axilt_arready = '1' then
+                    internal_arvalid <= '0';
+                end if; 
+                if s_axilt_rvalid = '1' and internal_rready = '1' then 
+                    internal_rready <= '0';
+                    mydata(counter) <= s_axilt_rdata((final_read*8)-1 downto 0);
+                    counter <= counter + 1;
+                    state <= END;
+                end if;
+            when END =>
+                    report "DATA READ ARE:"
+                    for i in 0 to counter-1 loop 
+                        report "DATA:" &to_string(mydata(i));
+                    end loop; 
+            end case; 
+            end if;
+            end process;
 s_axilt_arvalid <= internal_arvalid ;
 s_axilt_rready <= internal_rready ;
 
