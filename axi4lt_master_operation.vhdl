7c048f5d9a13f8a22151f95cc006806bf8f77e9d
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-07-28 12:40:03 +0300

    fixes error in clock cycles for wstrb, just initialized the correct value 1 cirrcle behind

--- axi4lt_master_operation.vhdl
+++ axi4lt_master_operation.vhdl
@@ -240,24 +240,23 @@ architecture behavioural of axi4_lite_master is
                     state_w <= WRITE;
                 elsif final_write /= 0 then
                     state_w <= FINAL_WRITING;
+                        case final_write is
+                            when 0 =>
+                            s_axilt_wstrb <= "0000";
+                            when 1 => 
+                            s_axilt_wstrb <= "0001";
+                            when 2 =>
+                            s_axilt_wstrb <= "0011";
+                            when 3 =>
+                            s_axilt_wstrb <= "0111";
+                            when others =>
+                            null;
+                        end case;
                 else state_w <= FINAL_W;
                 end if;
             else state_w <= WAITING_W; 
             end if;
             when FINAL_WRITING =>
-            case final_write is
-                when 0 =>
-                s_axilt_wstrb <= "0000";
-                when 1 => 
-                s_axilt_wstrb <= "0001";
-                when 2 =>
-                s_axilt_wstrb <= "0011";
-                when 3 =>
-                s_axilt_wstrb <= "0111";
-                when others =>
-                null;
-                end case;
-
                         if internal_awvalid = '1' and s_axilt_awready = '1' then
                             internal_awvalid <= '0';
                             aw_done <= '1';
